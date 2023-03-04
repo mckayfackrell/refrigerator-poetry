df8aa16687dabff8e405ece3edce08707bec5185
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-03 19:59:01 -0700

    added base files to get tailwindcss working

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,118 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [nav, setNav] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false); // new state variable to hold login status
+  const handleClick = () => setNav(!nav);
 
-export default Navbar
+  return (
+    // navbar
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#ffffff] text-black">
+      {/* logo */}
+      <div className="flex items-center">
+        <Link to="home" smooth={true} duration={500}>
+          <img
+            className="hover:cursor-pointer"
+            src={Logo}
+            alt="Fridge"
+            style={{ width: "60px" }}
+          />
+        </Link>
+        <div className="ml-2 text-xl md:text-2xl">Refrigerator Poetry</div>
+      </div>
+
+      {/* menu */}
+      <ul className="hidden md:flex">
+        <li className="hover:underline md:text-xl m-4">
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="hover:underline md:text-xl m-4">
+          <Link to="about" smooth={true} duration={500}>
+            Create
+          </Link>
+        </li>
+        <li className="hover:underline md:text-xl m-4">
+          <Link to="skills" smooth={true} duration={500}>
+            Dashboard
+          </Link>
+        </li>
+        {/* conditionally render "Login" or "Logout" link based on login status */}
+        {loggedIn ? (
+          <li className="hover:underline md:text-xl m-4">
+            <Link to="/" onClick={() => setLoggedIn(false)}>
+              Logout
+            </Link>
+          </li>
+        ) : (
+          <li className="hover:underline md:text-xl m-4">
+            <Link to="projects" smooth={true} duration={500}>
+              Login
+            </Link>
+          </li>
+        )}
+      </ul>
+
+      {/* hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* mobile menu */}
+      <ul
+        onClick={handleClick}
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#ffffff] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-3 text-2xl md:text-4xl hover:underline">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-3 text-2xl md:text-4xl hover:underline">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            Create
+          </Link>
+        </li>
+        <li className="py-3 text-2xl md:text-4xl hover:underline">
+          <Link
+            onClick={handleClick}
+            to="skills"
+            smooth={true}
+            duration={500}
+            offset={-80}
+          >
+            Dashboard
+          </Link>
+        </li>
+        {loggedIn ? (
+          <li className="py-3 text-2xl md:text-4xl hover:underline">
+            <Link to="/" onClick={() => setLoggedIn(false)}>
+              Logout
+            </Link>
+          </li>
+        ) : (
+          <li className="py-3 text-2xl md:text-4xl hover:underline">
+            <Link to="projects" smooth={true} duration={500}>
+              Login
+            </Link>
+          </li>
+        )}
+      </ul>
+
+      {/* footer */}
+      <footer class="fixed bottom-0 left-0 z-20 w-full p-2 bg-[#ffffff]md:flex md:items-center md:justify-between md:p-3">
+        <span class="text-sm text-black sm:text-center dark:black">© 2023</span>
+      </footer>
+    </div>
+  );
+};
+
+export default Navbar;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div name="home" className="w-full h-screen bg-[#ffffff]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+          <div className="flex flex-col justify-center">
+            <h1 className="text-4xl sm:text-7xl font-bold text-black">
+              Dashboard
+            </h1>
+            <p className="py-4 max-w-[700px] text-1xl lg:text-2xl">
+              This is where a post will go
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
