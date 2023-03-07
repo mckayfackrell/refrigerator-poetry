ab3e8a0715ff99440a85413a5990ed11acb8891c
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-06 20:14:32 -0700

    updated axios

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,4 +3,4 @@ import axios from "axios";
 const search = async (query) =>
   //axios.get(`https://api.datamuse.com/words?${query}&max=25`);
   axios.get("https://api.datamuse.com/words" + query + "&max=40");
-export default search;
+export default { search };
