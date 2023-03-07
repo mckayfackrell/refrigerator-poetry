0f22ba17bb5960d94cb22d468f50f61b2da58a33
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-06 21:19:37 -0700

    corrected style sheet

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
   return (
     <div className="card bg-white card-rounded w-50">
       <div className="card-header bg-dark text-center">
-        <h1 className="text-3xl">Fridge Poetry</h1>
       </div>
       <div className="card-body m-5">
         {loading}
