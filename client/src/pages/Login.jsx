e9ef195d27a150a0b19f877a96be4f21bacb502d
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-06 20:41:21 -0700

    updated login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,35 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-
-function Login() {
-  const handleReturnHome = () => {
-    window.location.href = "/register";
-  };
-
-  return (
-    <div name="home" className="w-full h-screen bg-[#ffffff]">
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <div className="grid grid-cols-1 gap-8 flex-col">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-            <div className="flex flex-col justify-center">
-              <h1 className="text-4xl sm:text-7xl font-bold text-black">
-                Login
-              </h1>
-              <p className="py-4 max-w-[700px] text-1xl lg:text-2xl">
-                This is where a user can login
-              </p>
-            </div>
-          </div>
-          <div className="mt-4 sm:mt-0 ">
-            <button
-              onClick={handleReturnHome}
-              className="text-black group bg-[#ffffff] border-[#000000] border-2 px-6 py-3 my-2 flex items-center hover:bg-[#f0f0f0]"
-            >
-              Register
-            </button>
-          </div>
-        </div>
-=======
 
 import magnetImg from "../assets/magnet.jpg";
 
@@ -63,13 +32,7 @@ export default function Login() {
           </p>
           <p className="text-center mt-8">Not a member? Sign up now</p>
         </form>
->>>>>>> feature/login
       </div>
     </div>
   );
 }
-<<<<<<< HEAD
-
-export default Login;
-=======
->>>>>>> feature/login
