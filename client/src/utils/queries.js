94f4ec0ba03bb135f7b0dc584a7c628b5b3fc0bd
benmday <benjaminmichaelday@gmail.com>
2023-03-07 12:02:40 -0800

    got comments to display under user posts

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,32 +11,19 @@ export const QUERY_USER = gql`
 `;
 
 export const QUERY_POSTS = gql`
-  query AllPosts {
-    allPosts {
+query AllPosts {
+  allPosts {
+    _id
+    postTitle
+    description
+    createdAt
+    comments {
       _id
-      postTitle
-      description
+      comment
+      createdAt
     }
   }
+}
 `;
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
-      _id
-      name
-    }
-  }
-`;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
