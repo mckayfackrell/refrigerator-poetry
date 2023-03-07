dae4557709b03e8b795a9895a28a70a7b0a10fae
benmday <benjaminmichaelday@gmail.com>
2023-03-06 20:41:37 -0800

    added comment button

--- client/src/pages/UserPosts.js
+++ client/src/pages/UserPosts.js
@@ -10,6 +10,7 @@ const UserPosts = ({ postList }) => {
               {poem.postTitle}
             </h4>
             <div dangerouslySetInnerHTML={{ __html: poem.description }} />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Comment</button>
           </div>
         ))}
     </div>
