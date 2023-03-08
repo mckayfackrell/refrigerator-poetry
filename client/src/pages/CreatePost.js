ff04a576138a246e2e6c5a11b75c20ad20eac01d
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-07 18:38:16 -0700

    updated css

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -43,43 +43,53 @@ const Matchup = () => {
   };
 
   return (
-    <div className="card bg-white card-rounded w-25">
-      <div className="card-header bg-dark text-center">
-        <h1>Let's create a matchup!</h1>
+    <main
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${magnetImg})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
+      <div className="card bg-white card-rounded w-25">
+        <div className="card-header bg-dark text-center">
+          <h1>Let's create a matchup!</h1>
+        </div>
+        <div className="card-body m-5">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <form onSubmit={handleFormSubmit}>
+              <label>Create your post: </label>
+              <select name="post-text" onChange={handleInputChange}>
+                {techList.map((tech) => {
+                  return (
+                    <option key={tech._id} value={tech.name}>
+                      {tech.name}
+                    </option>
+                  );
+                })}
+              </select>
+              <label>Tech 2: </label>
+              <select name="tech2" onChange={handleInputChange}>
+                {techList.map((tech) => {
+                  return (
+                    <option key={tech._id} value={tech.name}>
+                      {tech.name}
+                    </option>
+                  );
+                })}
+              </select>
+              <button className="btn btn-danger" type="submit">
+                Create Matchup!
+              </button>
+            </form>
+          )}
+        </div>
+        {error && <div>Something went wrong...</div>}
       </div>
-      <div className="card-body m-5">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <label>Create your post: </label>
-            <select name="post-text" onChange={handleInputChange}>
-              {techList.map((tech) => {
-                return (
-                  <option key={tech._id} value={tech.name}>
-                    {tech.name}
-                  </option>
-                );
-              })}
-            </select>
-            <label>Tech 2: </label>
-            <select name="tech2" onChange={handleInputChange}>
-              {techList.map((tech) => {
-                return (
-                  <option key={tech._id} value={tech.name}>
-                    {tech.name}
-                  </option>
-                );
-              })}
-            </select>
-            <button className="btn btn-danger" type="submit">
-              Create Matchup!
-            </button>
-          </form>
-        )}
-      </div>
-      {error && <div>Something went wrong...</div>}
-    </div>
+    </main>
   );
 };
 
--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -5,6 +5,7 @@ import { useMutation } from "@apollo/client";
 import { CREATE_POST } from "../utils/mutations";
 import API from "../utils/API";
 import WordList from "./WordList";
+import magnetImg from "../assets/magnet.jpg";
 
 const EditPost = (props) => {
   // Get words from Datamuse API
@@ -126,116 +127,133 @@ const EditPost = (props) => {
   };
 
   return (
-    <div className="poem-interface-outer">
-      <main>
-      <div className="create-a-post container">
-        <div className="collapse-outer">
-          <h3 className="collapse open">CLOSE X</h3>
-          <div id="words-finder" className="collapse-content">
-            <form id="words-finder-form" onSubmit={getWordsFromAPI}>
-              <div className="form-block display-flex justify-space-between query-builder form-group grid gap-2 grid-cols-4">
-                <h3>FIND WORDS:</h3>
+    <main
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${magnetImg})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100%",
+      }}
+    >
+      <div className="poem-interface-outer">
+        <main>
+          <div className="create-a-post container">
+            <div>
+              <br></br>
+              {/* <div className="collapse-outer"> */}
+              {/* <h3 className="collapse open">CLOSE X</h3> */}
+              <div id="words-finder" className="collapse-content">
+                <form id="words-finder-form" onSubmit={getWordsFromAPI}>
+                  <div className="form-block display-flex justify-space-between query-builder form-group grid gap-2 grid-cols-4">
+                    <h3>FIND WORDS:</h3>
 
-                <select
-                  name="listRule"
-                  id="words-behavior"
-                  className="select"
-                  onChange={handleAPIinputChange}
-                >
-                  <option value="rel_trg">about</option>
-                  <option value="ml">like</option>
-                  <option value="rel_rhy">rhyming with</option>
-                </select>
+                    <select
+                      name="listRule"
+                      id="words-behavior"
+                      className="select"
+                      onChange={handleAPIinputChange}
+                    >
+                      <option value="rel_trg">about</option>
+                      <option value="ml">like</option>
+                      <option value="rel_rhy">rhyming with</option>
+                    </select>
 
+                    <input
+                      name="listWord"
+                      type="text"
+                      id="words-subject"
+                      className="form-input"
+                      placeholder="subject"
+                      onChange={handleAPIinputChange}
+                    />
+                    <button id="find-words-btn" className="btn btn-submit">
+                      GO
+                    </button>
+                  </div>
+                </form>
+                <div id="word-list-dynamic">
+                  <WordList
+                    dmWordList={dynamicWordList}
+                    moveMagnet={moveMagnet}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              {/* <div className="collapse-outer"> */}
+              {/* <h3 className="collapse open">CLOSE X</h3> */}
+              <br></br>
+              <div className="collapse-content">
+                <div id="word-list-static">
+                  <div id="static-fragments">
+                    {props.staticfragments.map((item) => (
+                      <button
+                        className="magnet"
+                        key={item.id}
+                        onClick={() => moveMagnet(item.name)}
+                      >
+                        {item.name}
+                      </button>
+                    ))}
+                  </div>
+                  <div id="static-words">
+                    {props.staticwords.map((item) => (
+                      <button
+                        className="magnet"
+                        key={item.id}
+                        onClick={() => moveMagnet(item.name)}
+                      >
+                        {item.name}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div id="poem-container" className="empty">
+              <div dangerouslySetInnerHTML={{ __html: poemBody }} />
+            </div>
+
+            <div id="help-buttons" className="text-right">
+              <button
+                className="btn floating col-3"
+                data-func="start-over"
+                onClick={() => moveMagnet("start-over")}
+              >
+                START OVER
+              </button>
+            </div>
+
+            <form
+              className="form update-a-post-form"
+              data-id="usethepostidforthis"
+              data-func="save"
+              onSubmit={handleFormSubmit}
+            >
+              <div className="form-group display-flex justify-content-space-between">
                 <input
-                  name="listWord"
-                  type="text"
-                  id="words-subject"
                   className="form-input"
-                  placeholder="subject"
-                  onChange={handleAPIinputChange}
+                  type="text"
+                  id="newpost-title"
+                  name="postTitle"
+                  placeholder="title of your poem"
+                  onChange={handleInputChange}
                 />
-                <button id="find-words-btn" className="btn btn-submit">
-                  GO
+                <input type="hidden" name="description" />
+                <button className="btn btn-submit floating" type="submit">
+                  SAVE
                 </button>
               </div>
             </form>
-            <div id="word-list-dynamic">
-              <WordList dmWordList={dynamicWordList} moveMagnet={moveMagnet} />
-            </div>
           </div>
-        </div>
 
-        <div className="collapse-outer">
-          <h3 className="collapse open">CLOSE X</h3>
-          <div className="collapse-content">
-            <div id="word-list-static">
-              <div id="static-fragments">
-                {props.staticfragments.map((item) => (
-                  <button
-                    className="magnet"
-                    key={item.id}
-                    onClick={() => moveMagnet(item.name)}
-                  >
-                    {item.name}
-                  </button>
-                ))}
-              </div>
-              <div id="static-words">
-                {props.staticwords.map((item) => (
-                  <button
-                    className="magnet"
-                    key={item.id}
-                    onClick={() => moveMagnet(item.name)}
-                  >
-                    {item.name}
-                  </button>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div id="poem-container" className="empty">
-          <div dangerouslySetInnerHTML={{ __html: poemBody }} />
-        </div>
-
-        <div id="help-buttons" className="text-right">
-          <button
-            className="btn floating col-3"
-            data-func="start-over"
-            onClick={() => moveMagnet("start-over")}
-          >
-            START OVER
-          </button>
-        </div>
-
-        <form
-          className="form update-a-post-form"
-          data-id="usethepostidforthis"
-          data-func="save"
-          onSubmit={handleFormSubmit}
-        >
-          <div className="form-group display-flex justify-content-space-between">
-            <input
-              className="form-input"
-              type="text"
-              id="newpost-title"
-              name="postTitle"
-              placeholder="title of your poem"
-              onChange={handleInputChange}
-            />
-            <input type="hidden" name="description" />
-            <button className="btn btn-submit floating" type="submit">
-              SAVE
-            </button>
-          </div>
-        </form>
+          {error && <div>Something went wrong...</div>}
+        </main>
       </div>
-
-      {error && <div>Something went wrong...</div>}
     </main>
-    </div>
   );
 };
 
